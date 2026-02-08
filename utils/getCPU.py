ede53a9ed0eadc25fbc7aa14bfa3f8ed78e948f4
Subhadeep-Kumbhakar <kumbhakars669@gmail.com>
2026-02-08 23:06:48 +0530

    Fix Windows CPU detection

--- utils/getCPU.py
+++ utils/getCPU.py
@@ -1,11 +1,5 @@
-import ctypes
 import os
 
 def get_logical_cores():
-    lib_path = os.path.abspath("./utils/libcpu.so")
-    lib = ctypes.CDLL(lib_path)
-    lib.getCPUInfo.restype = ctypes.c_int
+    return os.cpu_count()
 
-    # Get the pointer and access the contents
-    cores = lib.getCPUInfo()
-    return cores
